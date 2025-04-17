4e0cbe41d6bac5565f517afc768bba896bf948a0
Thales Reis <thales.reisfurtado@gmail.com>
2025-04-17 11:44:19 -0300

    update

--- src/Components/Pages/Solicitacao/Solicitacao.jsx
+++ src/Components/Pages/Solicitacao/Solicitacao.jsx
@@ -9,6 +9,7 @@ import Deletar from "../../../assets/Solicitacao/deletar.png";
 import Lixeira from "../../../assets/Solicitacao/lixeira.png";
 import Motivo from "../../../assets/Solicitacao/motivo.png";
 
+
 export default function Solicitacao() {
 
   const [colaborador, setColaborador] = useState(""); // Estado para o campo colaborador
